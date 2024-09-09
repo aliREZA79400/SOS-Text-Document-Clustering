1c3ab563bca77ffd7439686cb16709a6c2f50a4a
aliREZA79400 <alireza1379prf99@gmail.com>
2024-09-09 08:40:50 +0330

    add tests for bbc news

--- scripts/main.py
+++ scripts/main.py
@@ -4,8 +4,8 @@ from typing import  Tuple
 from tqdm import tqdm
 import random
 
-from pso_pipline import pso_pipeline
-from sos_pipline import sos_pipeline
+from pso_pipline import pso_pipeline_20news , pso_pipeline_bbc_sport
+from sos_pipline import sos_pipeline_news , sos_pipeline_bbc_sport
 
 parser = argparse.ArgumentParser(description="Runner of Tests ")
 ### Parameters that set in shell
@@ -14,7 +14,7 @@ parser.add_argument("--num_data",type=int, help="number of sampls (int)")
 parser.add_argument("--file_name",type=str, help="file_name_to_save (str)")
 parser.add_argument("--algorithm_name",type=str, help="Algorith name : PSO | SOS (str)")
 parser.add_argument("--dataset_name",type=str, help="Dataset name : 20 News Group | BBC Sport (srt)")
-###
+### 
 
 # parser.add_argument("epoch" ,type=int, help="number of itrations(int)")
 # parser.add_argument("pop_size",type=int, help="number of population(int)")
@@ -22,7 +22,7 @@ parser.add_argument("--dataset_name",type=str, help="Dataset name : 20 News Grou
 args = parser.parse_args()
 
 ### Parameters that set in script
-hyper_parameters= ((500,10,30),(300,30,100),(160,55,170),(160,55,200),(120,80,300),(100,100,500),(120,55,None))
+hyper_parameters= ((100,10,30),(100,20,60),(100,30,100))
 
 all_categories_20news = ['alt.atheism',
  'comp.graphics',
@@ -45,7 +45,6 @@ all_categories_20news = ['alt.atheism',
  'talk.politics.misc',
  'talk.religion.misc']
 
-all_categories_BBC_Sport = []
 ###
 
 def apply_file(res_dict:dict,file_name :str):
@@ -57,24 +56,19 @@ def apply_file(res_dict:dict,file_name :str):
         print("file saved")
 
 
-def main_runner(DataSet:str,NUM_CLUSTERS:int,NUM_DATA:int,algorithm:str,HP:Tuple[tuple],file_name_to_save:str,NIA:int = 2):
+def main_runner_20news(NUM_CLUSTERS:int,NUM_DATA:int,algorithm:str,HP:Tuple[tuple],file_name_to_save:str,NIA:int = 2):
 
     results  = []
     for hy_pa in tqdm(HP):
-                
-                if DataSet == "20 News Group":    
-                    my_category = random.sample(all_categories_20news,NUM_CLUSTERS)
-                elif DataSet == "BBC Sport":
-                    my_category = random.sample(all_categories_BBC_Sport,NUM_CLUSTERS)
-                else : 
-                    raise ValueError("Dataset name is wrong!!! or does not exsit")
+                  
+                my_category = random.sample(all_categories_20news,NUM_CLUSTERS)
                
                 for i in range(NIA):
                     if algorithm == "PSO":
-                        g_best  ,start , elapsed  = pso_pipeline(dataset=DataSet , categories=my_category,K=NUM_CLUSTERS,NUM_SAMPLES = NUM_DATA,
+                        g_best  ,start , elapsed  = pso_pipeline_20news(categories=my_category,K=NUM_CLUSTERS,NUM_SAMPLES = NUM_DATA,
                                                         max_feature=hy_pa[2],epoch=hy_pa[0],pop_size=hy_pa[1])
                     elif algorithm == "SOS":
-                        g_best  ,start , elapsed  = sos_pipeline(dataset=DataSet , categories=my_category,K=NUM_CLUSTERS,NUM_SAMPLES = NUM_DATA,
+                        g_best  ,start , elapsed  = sos_pipeline_news(categories=my_category,K=NUM_CLUSTERS,NUM_SAMPLES = NUM_DATA,
                                                         max_feature=hy_pa[2],epoch=hy_pa[0],pop_size=hy_pa[1])
                     else :
                          raise ValueError("Algorithm name is wrong!!! or does not exsit")
@@ -94,8 +88,36 @@ def main_runner(DataSet:str,NUM_CLUSTERS:int,NUM_DATA:int,algorithm:str,HP:Tuple
     apply_file(results,file_name=file_name_to_save)
 
 
+def main_runner_bbc_sport(NUM_CLUSTERS:int,NUM_DATA:int,algorithm:str,HP:Tuple[tuple],file_name_to_save:str,NIA:int = 2):
+    results  = []
+    for hy_pa in tqdm(HP):               
+                for i in range(NIA):
+                    if algorithm == "PSO":
+                        g_best, elapsed  = pso_pipeline_bbc_sport(K=NUM_CLUSTERS,NUM_SAMPLES = NUM_DATA,
+                                                        max_feature=hy_pa[2],epoch=hy_pa[0],pop_size=hy_pa[1])
+                    elif algorithm == "SOS":
+                        g_best,elapsed  = sos_pipeline_bbc_sport(K=NUM_CLUSTERS,NUM_SAMPLES = NUM_DATA,
+                                                        max_feature=hy_pa[2],epoch=hy_pa[0],pop_size=hy_pa[1])
+                    else :
+                         raise ValueError("Algorithm name is wrong!!! or does not exsit")
+                    
+                    # Save results
+                    results.append({
+                        "hy_pa" : hy_pa , 
+                        "g_best" : g_best,
+                        "elapsed" : elapsed,
+                        "run" : i
+                    })
 
+                    print(f"hyper parameters {hy_pa}")
+
+    apply_file(results,file_name=file_name_to_save)
 
 if __name__ == "__main__":
-    main_runner(DataSet=args.d,NUM_CLUSTERS=args.cluster,NUM_DATA=args.n,algorithm=args.a,
-                HP=hyper_parameters,file_name_to_save=args.f)
+    if args.dataset_name == "20 News Group":
+        main_runner_20news(NUM_CLUSTERS=args.cluster,NUM_DATA=args.num_data,algorithm=args.algorithm_name,
+                    HP=hyper_parameters,file_name_to_save=args.file_name)
+    elif args.dataset_name == "BBC Sport" :
+        main_runner_bbc_sport(NUM_CLUSTERS=args.cluster,NUM_DATA=args.num_data,algorithm=args.algorithm_name,
+                    HP=hyper_parameters,file_name_to_save=args.file_name)
+         
--- scripts/pso_pipline.py
+++ scripts/pso_pipline.py
@@ -1,7 +1,12 @@
 import os
 # import numpy as np
 # from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.decomposition import TruncatedSVD
+
+from scipy.io import mmread
 import random
 import time
 from sklearn.datasets import  fetch_20newsgroups
@@ -13,14 +18,10 @@ from models import Data_Clustering_Purity_Obj_Func
 # from models import Data_Clustering_Purity_Obj_Func
 BASE_DIR = os.getcwd()
 
-def pso_pipeline(dataset=None,categories=None,K:int=None,NUM_SAMPLES:int=None,
+def pso_pipeline_20news(categories=None,K:int=None,NUM_SAMPLES:int=None,       
                    max_feature:int=None,epoch:int=None,pop_size:int=None):
 
-    if dataset == "20 News Group" :
-        X = fetch_20newsgroups(data_home=BASE_DIR, subset="all", categories=categories, shuffle=True, random_state=42, remove=('headers', 'footers', 'quotes'), download_if_missing=False, return_X_y=False)
-    elif dataset == "BBC Sport" :
-        pass
-
+    X = fetch_20newsgroups(data_home=BASE_DIR, subset="all", categories=categories, shuffle=True, random_state=42, remove=('headers', 'footers', 'quotes'), download_if_missing=False, return_X_y=False)
     n = len(X.data) # n = 4875
     start_index = random.sample(range(0,n-NUM_SAMPLES),1)  # like 4775
     preprocess_data = apply_preprocess_on_dataset(X.data[start_index[0]:start_index[0] +NUM_SAMPLES])
@@ -48,3 +49,61 @@ def pso_pipeline(dataset=None,categories=None,K:int=None,NUM_SAMPLES:int=None,
     elapsed  = end_time - start_time
 
     return g_best, start_index[0], elapsed
+
+
+######
+mtx_file_path = "/media/alireza/SSD/arshad_hosh/Thesis/ThesisCode/bbcsport/bbcsport.mtx"
+# targets
+trg = []
+with open("/media/alireza/SSD/arshad_hosh/Thesis/ThesisCode/bbcsport/bbcsport.classes" , "r") as f :
+    for line in f :
+        trg.append(int(line.split()[1]))
+
+
+def pso_pipeline_bbc_sport(K:int=None,NUM_SAMPLES:int=None,       
+                   max_feature:int=None,epoch:int=None,pop_size:int=None):
+    
+
+    # Read the mtx file into a scipy sparse matrix
+    numpy_mtx = mmread(mtx_file_path).toarray().transpose()
+
+    data_with_target =   list(zip(numpy_mtx , trg))
+    if K == 5 :
+        data_with_target = random.sample(data_with_target,NUM_SAMPLES)
+    else :
+        # class_labals = random.sample(range(5),K)
+        class_labals = [0,1]
+        data_with_target = [i for i in data_with_target if i[1] in class_labals]
+        data_with_target = random.sample(data_with_target,NUM_SAMPLES)
+
+    sampled_data , sampled_target = zip(*data_with_target)
+
+    sampled_data = np.array(list(sampled_data))
+
+    # Create a TfidfTransformer object
+    tfidf_transformer = TfidfTransformer()
+
+    # Fit the transformer to the count matrix and transform it to a TF-IDF matrix
+    sampled_data = tfidf_transformer.fit_transform(sampled_data)
+    svd = TruncatedSVD(n_components=max_feature)
+    X_new = svd.fit_transform(sampled_data.toarray())
+
+    bound = generate(K=K,dataset=X_new)
+
+    ### define problem with class {log training process}
+    problem_ins = Data_Clustering_Purity_Obj_Func(bounds=bound,
+                                                  K=K,
+                                                  target=list(sampled_target),
+                                                  dataset=X_new)
+
+
+    ### build model (instance)
+    model = PSO.OriginalPSO(epoch=epoch,pop_size=pop_size,c1=0.5,c2= 0.3,w=0.9)
+
+    ### train model  with solve ( training modes )
+    start_time = time.time()
+    g_best  = model.solve(problem=problem_ins)
+    end_time = time.time()
+    elapsed  = end_time - start_time
+
+    return g_best, elapsed
